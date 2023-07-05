65a45e7b00ee13199f704c192707d0b8fccec047
Sarah Geerling <tehsarah@gmail.com>
2023-07-04 20:19:06 -0500

    new updates made but still getting require errors

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const scriptHelper = require("./scriptHelper.js");
+let scriptHelper = require('./scriptHelper.js');
 
 window.addEventListener("load", function() {
-
    let listedPlanets;
-   let listedPlanetsResponse = myFetch();
+   let listedPlanetsResponse = scriptHelper.myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
         console.log(listedPlanets);
-        pickPlanet(listedPlanets);
+        let planet = scriptHelper.pickPlanet(listedPlanets);
+        let name = planet.name;
+        let diameter = planet.diameter;
+        let star = planet.star;
+        let distance = planet.distance;
+        let moons = planet.moons;
+        let imageUrl = planet.imageUrl;
+        scriptHelper.addDestinationInfo(planet, name, diameter, star, distance, moons, imageUrl);
     })
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,19 +1,19 @@
 // Write your helper functions here!
-const fetch = require('isomorphic-fetch');
+require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
+    const missionTarget = document.getElementById("missionTarget");
+        missionTarget.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">
+            `;
 }
 
 function validateInput(testInput) {
@@ -88,8 +88,10 @@ function pickPlanet(planets) {
     return planets;
 }
 
-module.exports.addDestinationInfo = addDestinationInfo;
-module.exports.validateInput = validateInput;
-module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
-module.exports.myFetch = myFetch;
+module.exports = {
+    addDestinationInfo: addDestinationInfo,
+    validateInput: validateInput,
+    formSubmission: formSubmission,
+    pickPlanet: pickPlanet,
+    myFetch: myFetch
+}
